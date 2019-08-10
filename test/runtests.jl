42276e1d02b9e1d89695e622757f42f8320cd7f3
Kyungdahm Yun <tomyun@gmail.com>
2019-08-10 14:39:32 -0700

    Clean up outdated comment

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,29 +8,6 @@ include("system.jl")
 @equation c(a, b) = a + b
 @equation d(b) = b
 
-# mutable struct MySystem <: System
-#     context::System
-#     parent::System
-#     children::Vector{System}
-#
-#     a::Var
-#     b::Var
-#     c::Var
-#     d::Var
-#
-#     function MySystem(;context, parent, children=System[])
-#         s = new()
-#         s.context = context
-#         s.parent = parent
-#         s.children = children
-#         s.a = Var(s, a, Track; name=:a, init=1, time=s.context.clock.time)
-#         s.b = Var(s, b, Track; name=:b, time=s.context.clock.time)
-#         s.c = Var(s, c, Track; name=:c, time=s.context.clock.time)
-#         s.d = Var(s, d, Accumulate; name=:d, time=s.context.clock.time)
-#         s
-#     end
-# end
-
 @system MySystem begin
     a ~ track
     b ~ track
@@ -51,11 +28,3 @@ end
 
 s = instance(MySystem)
 advance!(s.context)
-
-# print(convert(Float64, s.a))
-# print(promote(s.a, 1.0))
-# print(s.a + 1)
-#
-# import Base: exp
-# exp(x::Var) = exp(convert(Float64, x))
-# print(exp(s.a))
