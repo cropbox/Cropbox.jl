a24c16201089a8be0554f3f5c364fdd65724026b
Kyungdahm Yun <tomyun@gmail.com>
2019-08-15 22:20:40 -0700

    Parameterize Timepiece{T} with only T<:Number

--- src/time.jl
+++ src/time.jl
@@ -1,20 +1,20 @@
-mutable struct Timepiece{T}
+mutable struct Timepiece{T<:Number}
     t::T
 end
 
-advance!(timer::Timepiece{T}, t=one(T)) where T = (timer.t += t)
+advance!(timer::Timepiece{T}, t=one(T)) where {T<:Number} = (timer.t += t)
 reset!(timer::Timepiece) = (timer.t = 0)
-update!(timer::Timepiece{T}, t::T) where T = begin
+update!(timer::Timepiece{T}, t::T) where {T<:Number} = begin
     dt = t - timer.t
     dt > zero(T) && advance!(timer, dt)
     #TODO: make sure dt is not negative
     dt
 end
-update!(timer::Timepiece{T}, t) where T = update!(timer, convert(T, t))
+update!(timer::Timepiece{T}, t) where {T<:Number} = update!(timer, convert(T, t))
 
 import Base: convert, promote_rule, +
-convert(::Type{Timepiece{T}}, timer::Timepiece) where T = timer
-convert(::Type{T}, timer::Timepiece) where T = convert(T, timer.t)
+convert(::Type{Timepiece{T}}, timer::Timepiece) where {T<:Number} = timer
+convert(::Type{T}, timer::Timepiece) where {T<:Number} = convert(T, timer.t)
 # convert(::Type{Timepiece{T}}, t) where T = Timepiece(convert(T, t))
 # promote_rule(::Type{Timepiece{T}}, ::Type{U}) where {T,U} = promote_type(T, U)
 # +(timer::Timepiece, t) = +(promote(timer, t)...)
