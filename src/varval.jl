4fd729cf93bd93fd9afc48d8a239b3c236ec5149
Kyungdahm Yun <tomyun@gmail.com>
2019-08-17 01:37:05 -0700

    Remove unused parameterization of VarPath

--- src/varval.jl
+++ src/varval.jl
@@ -1,4 +1,4 @@
-struct VarPath{V}
+struct VarPath
     system::System
     path::Vector{Symbol}
 end
@@ -7,7 +7,7 @@ import Base: convert
 convert(::Type{Vector{Symbol}}, s::Symbol) = [s]
 convert(::Type{Vector{Symbol}}, s::String) = Symbol.(split(s, "."))
 
-VarPath{V}(s::System, p::Vector) where V = VarPath{V}(s, convert.(Vector{Symbol}, p) |> Iterators.flatten |> collect)
+VarPath(s::System, p::Vector) where V = VarPath(s, convert.(Vector{Symbol}, p) |> Iterators.flatten |> collect)
 
 getvar(p::VarPath) = reduce((a, b) -> getfield(a, b), [p.system; p.path])
 value!(p::VarPath) = value!(getvar(p))
@@ -16,11 +16,11 @@ convert(::Type{V}, p::VarPath) where {V<:Number} = convert(V, value!(p))
 convert(::Type{V}, p::VarPath) where {V<:Quantity} = convert(V, unitfy(value!(p), unit(V)))
 
 struct VarVal{V<:Number}
-    v::Union{VarPath{V},V}
+    v::Union{VarPath,V}
 end
 
-VarVal{V}(s::System, p::String) where {V<:Number} = VarVal{V}(VarPath{V}(s, p))
-VarVal{V}(s::System, p::String) where {V<:Quantity} = VarVal{V}(VarPath{V}(s, p))
+VarVal{V}(s::System, p::String) where {V<:Number} = VarVal{V}(VarPath(s, p))
+VarVal{V}(s::System, p::String) where {V<:Quantity} = VarVal{V}(VarPath(s, p))
 VarVal{V}(s::System, p) where {V<:Number} = VarVal{V}(convert(V, p))
 VarVal{V}(s::System, p) where {V<:Quantity} = VarVal{V}(convert(V, unitfy(p, unit(V))))
 VarVal{V}(s::System, ::Nothing) where {V<:Number} = nothing
