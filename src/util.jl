dec1f420b9f5677085d9b8f86eae2f3d1f1aec3d
Kyungdahm Yun <tomyun@gmail.com>
2019-08-23 15:52:26 -0700

    Update/fix typo

--- src/util.jl
+++ src/util.jl
@@ -1,5 +1,4 @@
 growing_degree_days(T, T_base; T_opt=nothing, T_max=nothing) = begin
-    isnothing(T) && return 0
     T = ustrip(unitfy(T, u"°C"))
     T_opt = !isnothing(T_opt) && ustrip(unitfy(T_opt, u"°C"))
     T_max = !isnothing(T_max) && ustrip(unitfy(T_max, u"°C"))
@@ -9,11 +8,10 @@ growing_degree_days(T, T_base; T_opt=nothing, T_max=nothing) = begin
 end
 
 beta_thermal_func(T, T_opt, T_max, T_min=0; beta=1) = begin
-    isnothing(T) && return 0
     T = ustrip(unitfy(T, u"°C"))
-    T_opt = !isnothing(T_opt) && ustrip(unitfy(T_opt, u"°C"))
-    T_max = !isnothing(T_max) && ustrip(unitfy(T_max, u"°C"))
-    T_min = !isnothing(T_max) && ustrip(unitfy(T_min, u"°C"))
+    T_opt = ustrip(unitfy(T_opt, u"°C"))
+    T_max = ustrip(unitfy(T_max, u"°C"))
+    T_min = ustrip(unitfy(T_min, u"°C"))
     !(T_min < T < T_max) && return 0
     !(T_min < T_opt < T_max) && return 0
     # beta function, See Yin et al. (1995), Ag For Meteorol., Yan and Hunt (1999) AnnBot, SK
@@ -26,9 +24,8 @@ beta_thermal_func(T, T_opt, T_max, T_min=0; beta=1) = begin
 end
 
 q10_thermal_func(T, T_opt; Q10=2) = begin
-    isnothing(T) && return 0
-    T_opt = !isnothing(T_opt) && ustrip(unitfy(T_opt, u"°C"))
     T = ustrip(unitfy(T, u"°C"))
+    T_opt = ustrip(unitfy(T_opt, u"°C"))
     Q10^((T - T_opt) / 10)
 end
 
