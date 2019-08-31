891eeb624dff48b640b2429674fe5daf27d858aa
Kyungdahm Yun <tomyun@gmail.com>
2019-08-31 16:08:54 -0700

    Remove unused update!

--- src/var.jl
+++ src/var.jl
@@ -172,7 +172,6 @@ getvars(v::Vector{<:System}, o::VarOpIndex, X) = getvar(v, o)
 getvars(v::Vector{<:System}, o::VarOpFilter, X) = getvar(v, o)
 getvars(s::Vector, n::Symbol, X) = (x = getvar.(s, n); pushvars!(X, x); x)
 
-update!(x::Var) = update!(x, MainStep())
 update!(x::Var, t::Step) = (s = state(x); queue!(system(x).context, store!(s, x, t), flushorder(s)))
 
 value(x::Var) = value(state(x))
