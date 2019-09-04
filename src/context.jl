2415dda7d2c357d8bf8f846d8442f88aa756e375
Kyungdahm Yun <tomyun@gmail.com>
2019-09-03 17:47:34 -0700

    Implement run!

--- src/context.jl
+++ src/context.jl
@@ -26,6 +26,19 @@ advance!(c::Context, n=1, reset=false) = begin
 end
 advance!(s::System, n=1) = advance!(s.context, n)
 
+import DataFrames: DataFrame
+run!(s::System, n=1; names...) = begin
+	N = (t="context.clock.tick", names...)
+	V = (k => [value(getproperty(s, n))] for (k, n) in pairs(N))
+	df = DataFrame(; V...)
+	for i in 1:n
+		advance!(s)
+		r = Tuple(value(getproperty(s, n)) for n in N)
+		push!(df, r)
+	end
+	df
+end
+
 instance(Ss::Type{<:System}...; config=configure()) = begin
     c = Context(; config=config)
     advance!(c, 1, true)
@@ -38,4 +51,4 @@ instance(Ss::Type{<:System}...; config=configure()) = begin
     c.systems[1]
 end
 
-export advance!, instance
+export advance!, run!, instance
