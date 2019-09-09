b1c086d5f1617f18bcaf276ce770c33c8e382972
Kyungdahm Yun <tomyun@gmail.com>
2019-09-08 21:36:58 -0700

    Remove old tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,29 +20,3 @@ end
     include("root_structure.jl")
     include("photosynthesis.jl")
 end
-
-@equation a() = 1
-@equation b(a) = a + 1
-@equation c(a, b) = a + b
-@equation d(b) = b
-
-@system MySystem begin
-    a ~ track
-    b ~ track
-    c ~ track
-    d: dd ~ accumulate
-end
-
-@system ASystem begin
-    a ~ track
-    b: bb ~ track(time="context.clock.tick")
-    ccc(a, b): c => a+b ~ track(unit=u"cm")
-    cccc(a=1, b=2): cc => a+b ~ track
-    d(a, b) => begin
-      a + b
-    end ~ track(cyclic)
-    e(a) => a ~ accumulate(init=0)
-end
-
-s = instance(MySystem)
-advance!(s.context)
