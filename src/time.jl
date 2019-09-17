30aa9362fca07b1483678779cfc27e24c2e269fd
Kyungdahm Yun <tomyun@gmail.com>
2019-09-17 13:26:37 -0700

    Remove no longere needed update! for Timepiece

--- src/time.jl
+++ src/time.jl
@@ -7,14 +7,6 @@ Timepiece{T}(t) where {T<:Number} = Timepiece{T}(t, oneunit(T))
 
 advance!(timer::Timepiece{T}, t) where {T<:Number} = (timer.t += t; timer.t)
 advance!(timer::Timepiece{T}) where {T<:Number} = advance!(timer, timer.dt)
-reset!(timer::Timepiece{T}) where {T<:Number} = (timer.t = zero(T))
-update!(timer::Timepiece{T}, t::T) where {T<:Number} = begin
-    dt = t - timer.t
-    (updated = dt > zero(T)) && advance!(timer, dt)
-    #TODO: make sure dt is not negative
-    updated
-end
-update!(timer::Timepiece{T}, t) where {T<:Number} = update!(timer, convert(T, t))
 
 import Base: convert, promote_rule, +
 convert(::Type{Timepiece{T}}, timer::Timepiece) where {T<:Number} = timer
