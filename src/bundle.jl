e54ab5b691a05689251f95e4946c56cbfef0ba1f
Kyungdahm Yun <tomyun@gmail.com>
2019-09-17 16:53:17 -0700

    Trying to add type info for Bundle/Bunch

--- src/bundle.jl
+++ src/bundle.jl
@@ -41,25 +41,25 @@ struct Bunch{S}
 end
 
 import Base: getproperty
-getproperty(b::Bundle, p::Symbol) = map(s -> getproperty(s, p), collect(b)) |> Bunch
-getproperty(b::Bunch, p::Symbol) = map(s -> getproperty(s, p), collect(b)) |> Bunch
+getproperty(b::Bundle, p::Symbol) = getfield.(collect(b), p) |> Bunch
+getproperty(b::Bunch, p::Symbol) = getfield.(collect(b), p) |> Bunch
 value(b::Bunch) = value.(collect(b))
 
 import Base: collect
-collect(b::Bundle) = reduce((a, b) -> collect(a, b), Any[getfield(b, :root), getfield(b, :ops)...])
+collect(b::Bundle{S}) where S = reduce((a, b) -> collect(a, b), Any[getfield(b, :root), getfield(b, :ops)...])::Vector{<:S}
 collect(b::Bunch) = getfield(b, :list)
 collect(p::Produce, ::BundleAll) = value(p)
-collect(p::Produce, ::BundleRecursiveAll) = begin
-    S = value(p)
-    l = System[]
+collect(p::Produce{S}, ::BundleRecursiveAll) where S = begin
+    V = value(p)
+    l = S[]
     #TODO: possibly reduce overhead by reusing calculated values in child nodes
-    f(S) = (append!(l, S); foreach(s -> f.(value(getproperty(s, p.name))), S); l)
-    f(S)
+    f(V) = (append!(l, V); foreach(s -> f.(value(getfield(s, p.name))), V); l)
+    f(V)
 end
-collect(S::Vector{<:System}, o::BundleIndex) = begin
-    n = length(S)
+collect(V::Vector{S}, o::BundleIndex) where {S<:System} = begin
+    n = length(V)
     i = o.index
     i = (i >= 0) ? i : n+i+1
-    (1 <= i <= n) ? [S[i]] : System[]
+    (1 <= i <= n) ? [V[i]] : S[]
 end
-collect(S::Vector{<:System}, o::BundleFilter) = filter(s -> value(getproperty(s, Symbol(o.cond))), S)
+collect(V::Vector{<:System}, o::BundleFilter) = filter(s -> value(getfield(s, Symbol(o.cond))), V)
