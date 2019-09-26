0b4c532ff33f42086d79ac875bbc653c8fba3a68
Kyungdahm Yun <tomyun@gmail.com>
2019-09-26 12:12:19 -0700

    Show alias with color

--- src/dive.jl
+++ src/dive.jl
@@ -1,41 +1,51 @@
 # REPL.TerminalMenus doesn't support single-option menu
 using TerminalMenus
+using Crayons.Box
 
 struct MenuItem
-    name::Symbol
+    name::String
+    label::String
     value
 end
 
+name(m::MenuItem) = m.name
+label(m::MenuItem) = isempty(m.label) ? m.name : m.label
+value(m::MenuItem) = m.value
+text(m::MenuItem) = begin
+    l = label(m)
+    v = value(m)
+    isempty(l) ? v : "$l $(DARK_GRAY_FG("=")) $v"
+end
+
 dive(s::System) = begin
-    nav(s::System, n) = nav(map(a -> (a, s[a]), [collectible(s)..., updatable(s)...]), n)
-    nav(s::Produce, n) = nav(collect(enumerate(value(s))), n)
-    nav(s::State, n) = nav(value(s), n)
-    nav(l::Vector, n) = begin
+    nav(s::System, t) = begin
+        d = Dict(fieldnamesalias(s))
+        label(k) = begin
+            n = string(k)
+            a = join(d[k], ", ")
+            isempty(a) ? "$(BLUE_FG(n))" : "$(BLUE_FG(a)) $(DARK_GRAY_FG("($n)"))"
+        end
+        nav(map(a -> MenuItem(string(a), label(a), s[a]), [collectible(s)..., updatable(s)...]), t)
+    end
+    nav(s::Produce, t) = nav(map(t -> MenuItem(string(t[1]), "", t[2]), enumerate(value(s))), t)
+    nav(s::State, t) = nav(map(v -> MenuItem("", "", v), value(s)), t)
+    nav(l::Vector{MenuItem}, t) = begin
         isempty(l) && return
-
-        label(v::Tuple) = string(v[1])
-        content(v::Tuple) = v[2]
-        item(v::Tuple) = "$(label(v)) = $(content(v))"
-
-        label(v) = ""
-        content(v) = v
-        item(v) = "$(content(v))"
-
         while true
-            println(n)
-            i = RadioMenu(item.(l), pagesize=40) |> request
+            println(t)
+            i = RadioMenu(text.(l), pagesize=40) |> request
             println()
             if i > 0
                 v = l[i]
-                nav(content(v), n * " > " * label(v))
+                nav(value(v), "$t $(DARK_GRAY_FG(">")) $(MAGENTA_FG(name(v)))")
             else
                 break
             end
         end
     end
-    nav(v, n) = throw(value(v))
+    nav(v, t) = throw(value(v))
     try
-        nav(s, name(s))
+        nav(s, string(MAGENTA_FG(name(s))))
     catch e
         !isa(e, InterruptException) && return e
     end
