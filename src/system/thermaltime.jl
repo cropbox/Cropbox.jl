ae779ed405bacc5d6196945592c58d34cca30276
Kyungdahm Yun <tomyun@gmail.com>
2019-10-29 00:51:27 -0700

    Reogranize with ThermalTime mix-in

--- src/system/thermaltime.jl
+++ src/system/thermaltime.jl
@@ -1,47 +1,52 @@
-@system GrowingDegreeDays begin
+@system ThermalTime begin
     temperature: T ~ track(u"°C", override)
+    timestep(context.clock.step): Δt ~ preserve(u"hr")
+    thermal_time: tt ~ track(u"hr^-1")
+end
 
+@system GrowingDegree(ThermalTime) begin
     base_temperature: Tb ~ preserve(u"°C", parameter)
     optimal_temperature: To ~ preserve(u"°C", optional, parameter)
     maximum_temperature: Tx ~ preserve(u"°C", optional, parameter)
 
-    thermal_time(T=nounit(T), Tb=nounit(Tb), To=nounit(To), Tx=nounit(Tx)): tt => begin
+    thermal_time(T, Tb, To, Tx, Δt): tt => begin
         T = !isnothing(To) ? min(T, To) : T
         T = !isnothing(Tx) && T >= Tx ? Tb : T
-        max(T - Tb, 0.)
-    end ~ track
+        ΔT = max(T - Tb, zero(T))
+        ΔT / Δt
+    end ~ track(u"K/hr")
 end
 
-@system BetaFunction begin
-    temperature: T ~ track(u"°C", override)
-
+@system BetaFunction(ThermalTime) begin
     minimum_temperature: Tn => 0 ~ preserve(u"°C", parameter)
     optimal_temperature: To ~ preserve(u"°C", parameter)
     maximum_temperature: Tx ~ preserve(u"°C", parameter)
     beta: β => 1 ~ preserve(parameter)
 
-    thermal_time(T=nounit(T), Tn=nounit(Tn), To=nounit(To), Tx=nounit(Tx), β): tt => begin
-        !(Tn < T < Tx) && return 0.
-        !(Tn < To < Tx) && return 0.
+    thermal_time(T, Tn, To, Tx, β, Δt): tt => begin
         # beta function, See Yin et al. (1995), Ag For Meteorol., Yan and Hunt (1999) AnnBot, SK
-        Ton = To - Tn
-        Txo = Tx - To
-        f = (T - Tn) / Ton
-        g = (Tx - T) / Txo
-        α = β * Ton / Txo
-        f^α * g^β
-    end ~ track
+        ΔT = if (Tn < T < Tx) && (Tn < To < Tx)
+            Ton = To - Tn
+            Txo = Tx - To
+            f = (T - Tn) / Ton
+            g = (Tx - T) / Txo
+            α = β * (Ton / Txo)
+            f^α * g^β
+        else
+            0.
+        end
+        ΔT / Δt
+    end ~ track(u"hr^-1")
 end
 
-@system Q10Function begin
-    temperature: T ~ track(u"°C", override)
-
+@system Q10Function(ThermalTime) begin
     optimal_temperature: To ~ preserve(u"°C", parameter)
     Q10 => 2 ~ preserve(parameter)
 
-    thermal_time(T=nounit(T), To=nounit(To), Q10): tt => begin
-        Q10^((T - To) / 10)
+    #FIXME: Q10 isn't actually a thermal function like others (not a rate, check unit)
+    thermal_time(T, To, Q10): tt => begin
+        Q10^((T - To) / 10u"K")
     end ~ track
 end
 
-export GrowingDegreeDays, BetaFunction, Q10Function
+export GrowingDegree, BetaFunction, Q10Function
