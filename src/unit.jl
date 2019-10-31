4755d789cba0e07d66dee252936e49735d22666f
Kyungdahm Yun <tomyun@gmail.com>
2019-10-30 17:27:40 -0700

    Add unitfy/deunitfy() for Tuple

--- src/unit.jl
+++ src/unit.jl
@@ -6,6 +6,7 @@ unitfy(::Nothing, ::Nothing) = nothing
 unitfy(v, ::Nothing) = v
 unitfy(v::Number, u::Units) = Quantity(v, u)
 unitfy(v::Array, u::Units) = Quantity.(v, u)
+unitfy(v::Tuple, u::Units) = Quantity.(v, u)
 unitfy(v::Quantity, u::Units) = uconvert(u, v)
 unitfy(v::Array{<:Quantity}, u::Units) = uconvert.(u, v)
 
@@ -13,6 +14,7 @@ deunitfy(::Nothing, u=nothing) = nothing
 deunitfy(v) = v
 deunitfy(v::Quantity) = ustrip(v)
 deunitfy(v::Array) = deunitfy.(v)
+deunitfy(v::Tuple) = deunitfy.(v)
 deunitfy(v, u::Units) = deunitfy(unitfy(v, u))
 
 # unitstr(s::String) = @eval @u_str $s
