1a632cf88df5042981e110bd716a5df82fc9fc1d
Kyungdahm Yun <tomyun@gmail.com>
2019-10-31 14:06:44 -0700

    Implement root structure visualization

--- test/root_structure.jl
+++ test/root_structure.jl
@@ -1,16 +1,20 @@
 using Distributions
 using Unitful
 
+using MeshCat
+import GeometryTypes: Cylinder3, Point3f0
+import CoordinateTransformations: AngleAxis, LinearMap, Translation
+
 @testset "root structure" begin
     @system RootSegment begin
         parent ~ ::Union{System,Nothing}(override)
         elongation_rate: r => rand(Normal(1, 0.2)) ~ track(u"mm/hr")
-        branching_angle => rand(Normal(20, 10))*u"°" ~ preserve(u"°")
+        branching_angle: a => rand(Normal(20, 10))*u"°" ~ preserve(u"°")
         branching_interval: i => 3.0 ~ track(u"mm")
         branching_chance: p => clamp(rand(Normal(0.5, 0.5)), 0, 1) ~ track
         is_branching(l, ll, i, p) => (l - ll > i && p > 0.5) ~ flag
         branched_length: bl => 0 ~ preserve(u"mm", extern)
-        diameter => 0.1 ~ track(u"mm")
+        diameter: d => 0.1 ~ track(u"mm")
         length(r): l ~ accumulate(u"mm")
         last_branching_length(x=branch["*/-1"].bl): ll => (isempty(x) ? 0. : x[1]) ~ track(u"mm")
         branch(is_branching, l) => begin
@@ -26,43 +30,41 @@ using Unitful
     end
 
     render(r::RootSegment) = begin
-        # s = trimesh.scene.scene.Scene()
-        # #TODO: make System's own walker method?
-        # def visit(r, pn=None):
-        #     l = U.magnitude(r.length, 'cm')
-        #     if l == 0:
-        #         return
-        #     m = trimesh.creation.cylinder(radius=U.magnitude(r.diameter, 'cm'), height=l, sections=4)
-        #     if pn is None:
-        #         m.visual.face_colors = (255, 0, 0, 255)
-        #     # put segment end at origin
-        #     m.apply_translation((0, 0, l/2))
-        #     # put root end at parent's tip
-        #     T1 = trimesh.transformations.translation_matrix((0, 0, -l))
-        #     # rotate root segment along random axis (x: random, y: left or right)
-        #     angle = U.magnitude(r.branching_angle, 'rad')
-        #     direction = (random.random(), (random.random() > 0.5) * 2 - 1, 0)
-        #     R = trimesh.transformations.rotation_matrix(angle, direction)
-        #     # put root segment at parent's branching point
-        #     z = 0 if pn is None else U.magnitude(r.parent.length - r.branched_length, 'cm')
-        #     T2 = trimesh.transformations.translation_matrix((0, 0, z))
-        #     M = trimesh.transformations.concatenate_matrices(T2, R, T1)
-        #     # add root segment
-        #     n = s.add_geometry(m, parent_node_name=pn, transform=M)
-        #     # visit recursively
-        #     [visit(cr, n) for cr in r.children]
-        # visit(self)
-        # s.show()
-        # s
+        i = 0
+        visit!(v, r) = begin
+            l = r.l' |> Cropbox.deunitfy
+            iszero(l) && return
+            d = Cropbox.deunitfy(r.d')
+            m = Cylinder3{Float32}(Point3f0(0), Point3f0(0, 0, l), d)
+            # put root end at parent's tip
+            T1 = Translation(0, 0, -l)
+            # rotate root segment along random axis (x: random, y: left or right)
+            a = Cropbox.deunitfy(r.a', u"rad")
+            R = AngleAxis(a, rand(), 2(rand() > 0.5) - 1, 0) |> LinearMap
+            # put root segment at parent's branching point
+            z = isnothing(r.parent) ? 0 : Cropbox.deunitfy(r.parent.l' - r.bl')
+            T2 = Translation(0, 0, z)
+            M = T2 ∘ R ∘ T1
+            # add root segment
+            vv = v["$i"]
+            i += 1
+            setobject!(vv, m)
+            settransform!(vv, M)
+            # visit recursively
+            for cr in r.branch
+                visit!(vv, cr)
+            end
+        end
+        v = Visualizer()
+        visit!(v, r)
+        v
     end
 
     s = instance(Root)
-    #d = []
-    while s.context.clock.tick' <= 30u"hr"
+    t = 30u"hr"
+    while s.context.clock.tick' <= t
         update!(s)
-        #push!(d, (transform(collect(r))))
     end
-    @test s.context.clock.tick' > 30u"hr"
-    #render(r)
-    #write(d, tmp_path/'root.json')
+    @test s.context.clock.tick' > t
+    #render(s.root) |> open
 end
