72744b89cbed41c05ffeb0b36e85fdd268c29cae
Kyungdahm Yun <tomyun@gmail.com>
2019-11-01 11:43:32 -0700

    Remove dependency on explicit timestep variable (use config instead)

--- test/lotka_volterra.jl
+++ test/lotka_volterra.jl
@@ -1,28 +1,30 @@
 @testset "lotka volterra" begin
     @system LotkaVolterra(Controller) begin
-        timestep(t=context.clock.tick): t => 0.01t ~ track(u"hr")
         prey_birth_rate: a => 1.0 ~ track(u"hr^-1")
         prey_death_rate: b => 0.1 ~ track(u"hr^-1")
         predator_death_rate: c => 1.5 ~ track(u"hr^-1")
         predator_reproduction_rate: d => 0.75 ~ track
         prey_initial_population: H0 => 10.0 ~ track
         predator_initial_population: P0 => 5.0 ~ track
-        prey_population(a, b, H, P): H => a*H - b*H*P ~ accumulate(init=H0, time=t)
-        predator_population(b, c, d, H, P): P => d*b*H*P - c*P ~ accumulate(init=P0, time=t)
+        prey_population(a, b, H, P): H => a*H - b*H*P ~ accumulate(init=H0)
+        predator_population(b, c, d, H, P): P => d*b*H*P - c*P ~ accumulate(init=P0)
     end
-    s = instance(LotkaVolterra)
+    s = instance(LotkaVolterra, config=(
+        :Clock => (:step => 1u"minute"),
+    ))
     T = Float64[]
     H = Float64[]
     P = Float64[]
     #TODO: isless() for Var with proper promote_rule
-    while s.t' <= 20.0u"hr"
+    t = s.context.clock.tick
+    while t' <= 20.0u"hr"
         #println("t = $(s.t): H = $(s.H), P = $(s.P)")
-        push!(T, s.t' |> ustrip)
+        push!(T, t' |> ustrip)
         push!(H, s.H')
         push!(P, s.P')
         update!(s)
     end
-    @test s.t' > 20.0u"hr"
+    @test t' > 20.0u"hr"
     using Plots
     unicodeplots()
     plot(T, [H P], lab=["Prey" "Predator"], xlab="Time", ylab="Population", xlim=(0, T[end]), ylim=(0, ceil(maximum([H P]))))
