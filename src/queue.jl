4ba2ea0df4c32a929f776d5228572abd00b3d81c
Kyungdahm Yun <tomyun@gmail.com>
2019-11-04 21:21:47 -0800

    Remove no longer used pending feature

--- src/queue.jl
+++ src/queue.jl
@@ -2,35 +2,19 @@
 struct Queue
     pre::Vector{Any}
     post::Vector{Any}
-    pending::Bool
-    prepending::Vector{Any}
-    postpending::Vector{Any}
 end
 
-Queue() = Queue(Any[], Any[], false, Any[], Any[])
+Queue() = Queue(Any[], Any[])
 
-current(q::Queue, ::PrePriority) = (q.pending ? q.prepending : q.pre)
-current(q::Queue, ::PostPriority) = (q.pending ? q.postpending : q.post)
+current(q::Queue, ::PrePriority) = q.pre
+current(q::Queue, ::PostPriority) = q.post
 
-queue!(q::Queue, f, p::PrePriority) = queue!(current(q, p), f)
-queue!(q::Queue, f, p::PostPriority) = queue!(current(q, p), f)
+queue!(q::Queue, f, p::Priority) = queue!(current(q, p), f)
 queue!(q::Vector, f) = push!(q, f)
 queue!(q::Vector, ::Nothing) = nothing
 
-flush!(q::Queue, p::PrePriority) = flush!(current(q, p))
-flush!(q::Queue, p::PostPriority) = flush!(current(q, p))
+flush!(q::Queue, p::Priority) = flush!(current(q, p))
 flush!(q::Vector) = (foreach(f -> f(), q); empty!(q))
 
 preflush!(q::Queue) = flush!(q, PrePriority())
 postflush!(q::Queue) = flush!(q, PostPriority())
-
-startpending(q::Queue) = (q.pending = true)
-stoppending(q::Queue; merge=false) = begin
-    q.pending = false
-    if merge
-        append!(q.pre, q.prepending)
-        append!(q.post, q.postpending)
-    end
-    empty!(q.prepending)
-    empty!(q.postpending)
-end
