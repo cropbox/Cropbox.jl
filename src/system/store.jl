19d57782d228a26158dd5a7f5c8c11b499bb2bdf
Kyungdahm Yun <tomyun@gmail.com>
2020-01-04 11:54:15 -0800

    Support simple indexing with optional indexkey

--- src/system/store.jl
+++ src/system/store.jl
@@ -3,19 +3,25 @@ import CSV
 
 @system DataFrameStore begin
     filename => "" ~ preserve::String(parameter)
-    indexkey => :timestamp ~ preserve::Symbol(parameter)
+    indexkey => :timestamp ~ preserve::Symbol(optional, parameter)
 
     index ~ hold
     timestamp ~ hold
 
     dataframe(filename, indexkey, timestamp): df => begin
         df = CSV.read(filename)
-        df[!, indexkey] = map(timestamp, eachrow(df))
+        if !isnothing(indexkey)
+            df[!, indexkey] = map(timestamp, eachrow(df))
+        end
         df
     end ~ preserve::DataFrame(extern, parameter)
 
     store(df, indexkey, index): s => begin
-        df[df[!, indexkey] .== index, :][1, :]
+        if !isnothing(indexkey)
+            df[df[!, indexkey] .== index, :][1, :]
+        else
+            df[index, :]
+        end
     end ~ track::DataFrameRow{DataFrame,DataFrames.Index}
 end
 
