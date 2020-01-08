062c39ff85051ae66f8218375761e51ac4f508cd
Kyungdahm Yun <tomyun@gmail.com>
2020-01-07 16:05:51 -0800

    Remove external default() function to populate ZonedDateTime init

--- src/system/calendar.jl
+++ src/system/calendar.jl
@@ -2,10 +2,8 @@ using TimeZones
 import Dates
 
 @system Calendar begin
-    init: t0 => default(ZonedDateTime) ~ preserve::ZonedDateTime(parameter)
+    init: t0 => ZonedDateTime(2017, 7, 16, tz"America/Los_Angeles") ~ preserve::ZonedDateTime(parameter)
     time(t0, tick=context.clock.tick): t => t0 + (ustrip(u"s", tick) |> Dates.Second) ~ track::ZonedDateTime
 end
-
-default(::Type{ZonedDateTime}) = ZonedDateTime(2017, 7, 16, tz"America/Los_Angeles")
 
 export Calendar
