59239a3d1a81c0834db44f14ac46563fa0d74287
Kyungdahm Yun <tomyun@gmail.com>
2020-01-14 14:05:56 -0800

    Keep rows/columns symbols in Tabulate State

--- src/state/tabulate.jl
+++ src/state/tabulate.jl
@@ -1,5 +1,7 @@
 struct Tabulate{V} <: State{V}
     value::Dict{Symbol,Dict{Symbol,V}}
+    rows::Tuple{Vararg{Symbol}}
+    columns::Tuple{Vararg{Symbol}}
 end
 
 Tabulate(; unit, rows, columns, _value, _type, _...) = begin
@@ -7,7 +9,7 @@ Tabulate(; unit, rows, columns, _value, _type, _...) = begin
     V = valuetype(_type, U)
     matrix2dict(m, r, c, V) = Dict(zip(r, [Dict(zip(c, V.(m[i,:]))) for i in 1:size(m, 1)]))
     v = matrix2dict(_value, rows, columns, V)
-    Tabulate{V}(v)
+    Tabulate{V}(v, rows, columns)
 end
 
 genvartype(v::VarInfo, ::Val{:Tabulate}; V, _...) = @q Tabulate{$V}
