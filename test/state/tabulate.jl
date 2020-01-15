1cd32f84a1914fafacdc1978a93cc9edd58f2cf8
Kyungdahm Yun <tomyun@gmail.com>
2020-01-14 16:28:28 -0800

    Add test case for Tabulate with parameter

--- test/state/tabulate.jl
+++ test/state/tabulate.jl
@@ -24,6 +24,32 @@
         @test s.Cb' == 5
     end
 
+    @testset "parameter" begin
+        @system STabulateParameter(Controller) begin
+            T ~ tabulate(rows=(:A, :B, :C), columns=(:a, :b), parameter)
+            p(T; c::Symbol, r::Symbol) => T[c][r] ~ call
+            Aa(p) => p(:A, :a) ~ track
+            Ba(p) => p(:B, :a) ~ track
+            Ca(p) => p(:C, :a) ~ track
+            Ab(p) => p(:A, :b) ~ track
+            Bb(p) => p(:B, :b) ~ track
+            Cb(p) => p(:C, :b) ~ track
+        end
+        o = STabulateParameter => :T => [
+            # a b
+              0 3 ; # A
+              1 4 ; # B
+              2 5 ; # C
+        ]
+        s = instance(STabulateParameter, config=o)
+        @test s.Aa' == 0
+        @test s.Ba' == 1
+        @test s.Ca' == 2
+        @test s.Ab' == 3
+        @test s.Bb' == 4
+        @test s.Cb' == 5
+    end
+
     @testset "default columns" begin
         @system STabulateDefaultColumns(Controller) begin
             T => [
