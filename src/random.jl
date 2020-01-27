61ad919abef7bbd14df50be4785bd2a161d63122
Kyungdahm Yun <tomyun@gmail.com>
2020-01-26 16:35:00 -0800

    Implement own Measurement struct and ± function

--- src/random.jl
+++ src/random.jl
@@ -1,11 +1,29 @@
 import Distributions: Distribution, Normal
-import Measurements
-import Measurements: Measurement, ±
+#HACK: Measurements.jl causes weird StackOverFlow error when used with other packages (i.e. Plots, UnicodePlots)
+#HACK: not exactly same, but maybe related with https://github.com/PumasAI/Pumas.jl/issues/609
+#import Measurements
+#import Measurements: Measurement, ±
 import Unitful: Quantity
 
+#HACK: define own Measurement just for supporting ± syntax
+struct Measurement{T} <: Number
+    val::T
+    err::T
+end
+
+measurement(a::Quantity, b::Quantity) = begin
+    u = Unitful.promote_unit(unit(a), unit(b))
+    measurement(ustrip(u, a), ustrip(u, b)) * u
+end
+measurement(a, b) = Measurement(promote(a, b)...)
+
+const ± = measurement
+export ±
+
+import Base: show
+show(io::IO, m::Measurement) = print(io, "$(m.val) ± $(m.err)")
+
 sample(v::Distribution) = rand(v)
-sample(v::Measurement) = rand(Normal(Measurements.value(v), Measurements.uncertainty(v)))
+sample(v::Measurement) = rand(Normal(v.val, v.err))
 sample(v::Quantity{<:Measurement}) = sample(ustrip(v)) * unit(v)
 sample(v) = v
-
-export ±
