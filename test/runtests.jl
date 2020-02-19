bebe26e276a9e6e13b80d1015bf069df1a00a95e
Kyungdahm Yun <tomyun@gmail.com>
2020-02-18 18:27:01 -0800

    Reorganize test suite including application models

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,19 @@ using Cropbox
 using Test
 
 @testset "cropbox" begin
-    include("macro.jl")
-    include("state.jl")
-    include("system.jl")
-    include("unit.jl")
-    include("config.jl")
-    include("tool.jl")
-    include("lotka_volterra.jl")
-    include("root_structure.jl")
-    include("photosynthesis.jl")
+    @testset "framework" begin
+        include("macro.jl")
+        include("state.jl")
+        include("system.jl")
+        include("unit.jl")
+        include("config.jl")
+        include("tool.jl")
+    end
+
+    @testset "application" begin
+        include("lotka_volterra.jl")
+        include("root/root.jl")
+        include("garlic/garlic.jl")
+        include("photosynthesis.jl")
+    end
 end
