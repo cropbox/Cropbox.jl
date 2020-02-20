59bac990cc21c91df3a94376dd9486fa002dd77b
Kyungdahm Yun <tomyun@gmail.com>
2020-02-19 21:13:47 -0800

    Remove short name for simplicity

--- src/system/calendar.jl
+++ src/system/calendar.jl
@@ -2,8 +2,8 @@ using TimeZones
 import Dates
 
 @system Calendar begin
-    t0: init => ZonedDateTime(2017, 7, 16, tz"America/Los_Angeles") ~ preserve::ZonedDateTime(extern, parameter)
-    t(t0, Δt=context.clock.tick): time => t0 + (Cropbox.deunitfy(Δt, u"s") |> Dates.Second) ~ track::ZonedDateTime
+    init => ZonedDateTime(2017, 7, 16, tz"America/Los_Angeles") ~ preserve::ZonedDateTime(extern, parameter)
+    time(t0=init, t=context.clock.tick) => t0 + (Cropbox.deunitfy(t, u"s") |> Dates.Second) ~ track::ZonedDateTime
 end
 
 export Calendar
