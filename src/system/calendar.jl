e2e8d2e8f0adba65577014a15845ac93e7e130fb
Kyungdahm Yun <tomyun@gmail.com>
2020-02-19 21:15:30 -0800

    Add last/stop

--- src/system/calendar.jl
+++ src/system/calendar.jl
@@ -3,7 +3,11 @@ import Dates
 
 @system Calendar begin
     init => ZonedDateTime(2017, 7, 16, tz"America/Los_Angeles") ~ preserve::ZonedDateTime(extern, parameter)
+    last => nothing ~ preserve::ZonedDateTime(extern, parameter, optional)
     time(t0=init, t=context.clock.tick) => t0 + (Cropbox.deunitfy(t, u"s") |> Dates.Second) ~ track::ZonedDateTime
+    stop(time, last) => begin
+        isnothing(last) ? false : (time >= last)
+    end ~ track::Bool
 end
 
 export Calendar
--- test/system/calendar.jl
+++ test/system/calendar.jl
@@ -13,4 +13,21 @@ import Dates
         update!(s)
         @test s.time' == t0 + Dates.Hour(2)
     end
+    
+    @testset "stop" begin
+        @system SCalendarStop(Calendar, Controller)
+        t0 = ZonedDateTime(2011, 10, 29, tz"Asia/Seoul")
+        t1 = t0 + Dates.Day(1)
+        o = :Calendar => (init=t0, last=t1)
+        s = instance(SCalendarStop; config=o)
+        @test s.init' == t0
+        @test s.last' == t1
+        @test s.time' == t0 + Dates.Hour(1)
+        @test s.stop' == false
+        for i in 1:24
+            update!(s)
+        end
+        @test s.time' == t1 + Dates.Hour(1)
+        @test s.stop' == true
+    end
 end
