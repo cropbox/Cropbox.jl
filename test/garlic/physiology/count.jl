6441fc346d3a9e5773db2faef0decaf8cc4a1f48
Kyungdahm Yun <tomyun@gmail.com>
2020-02-19 20:37:57 -0800

    Add leaves_initiated/appeared

--- test/garlic/physiology/count.jl
+++ test/garlic/physiology/count.jl
@@ -1,5 +1,9 @@
 @system Count begin
+    pheno: phenology ~ hold
     NU: nodal_units ~ hold
+
+    leaves_initiated(pheno.leaves_initiated) ~ track::Int
+    leaves_appeared(pheno.leaves_appeared) ~ track::Int
 
     leaves_growing(x=NU["*"].leaf.growing) => sum(x) ~ track::Int
     leaves_mature(x=NU["*"].leaf.mature) => sum(x) ~ track::Int
