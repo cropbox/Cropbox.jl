a8245e41869ba956e5ab44fe300cd7317df032f3
Kyungdahm Yun <tomyun@gmail.com>
2020-03-01 17:42:11 -0800

    Remove no longer relevant test case (covered by solve)

--- test/state/bisect.jl
+++ test/state/bisect.jl
@@ -7,14 +7,6 @@
         @test s.x' == 1
     end
 
-    # @testset "order0" begin
-    #     @system SolveOrder0 begin
-    #         x(x) => ((x^2 + 1) / 2) ~ solve
-    #     end
-    #     s = instance(SolveOrder0)
-    #     @test isapprox(s.x', 1; atol=1e-3)
-    # end
-
     @testset "bisect with unit" begin
         @system SBisectUnit(Controller) begin
             x(x) => 2x - u"1m" ~ bisect(lower=u"0m", upper=u"2m", u"m")
