6fe37d0cdc3814bea9165cfb01ec1a6cd363b456
Kyungdahm Yun <tomyun@gmail.com>
2020-03-03 14:06:27 -0800

    Use faster linear/quadratic solver

--- src/state/solve.jl
+++ src/state/solve.jl
@@ -29,63 +29,25 @@ end
 genpolynomialunits(U, n) = [@q($U^$(i-1)) for i in n:-1:1]
 
 import PolynomialRoots
-# solvepolynomial(p, pu, u) = begin
-#     sp = [deunitfy(q, qu) for (q, qu) in zip(p, pu)]
-#     r = PolynomialRoots.roots(sp)
-#     #unitfy(real.(filter(isreal, r)), u)
-#     real.(filter(isreal, r))
-# end
-# solvepolynomial(p, pu, u) = begin
-#     sp = [deunitfy(q, qu) for (q, qu) in zip(p, pu)]
-#     r = quadratic_solve(sp...)
-#     #unitfy(r, u)
-#     r
-# end
-solvepolynomial(p, pu, u, o) = begin
+solvepolynomial(p, pu) = begin
     sp = [deunitfy(q, qu) for (q, qu) in zip(p, pu)]
-    r = if o == 1
-        quadratic_solve_upper(sp...)
-    elseif o == 2
-        quadratic_solve_lower(sp...)
-    end
-    #unitfy(r, u)
-    r
+    r = PolynomialRoots.roots(sp)
+    real.(filter(isreal, r))
 end
-
-quadratic_solve(c, b, a) = begin
-    (a == 0) && return [0.]
-    v = b^2 - 4a*c
-    if v < 0
-        [-b/a]
+solvequadratic(a, b, c) = begin
+    (a == 0) && return (-c/b,)
+    Δ = b^2 - 4a*c
+    if Δ > 0
+        s = √Δ
+        ((-b - s) / 2a, (-b + s) / 2a)
+    elseif Δ == 0
+        (-b/2a,)
     else
-        sv = sqrt(v)
-        [(-b - sv) / 2a, (-b + sv) / 2a]
+        error("complex roots found for quadratic equation: $a*x^2 + $b*x + $c = 0")
     end
-end
-quadratic_solve2(c, b, a) = begin
-    (a == 0) && return (0.,)
-    v = b^2 - 4a*c
-    if v < 0
-        (-b/a,)
-    else
-        sv = sqrt(v)
-        ((-b - sv) / 2a, (-b + sv) / 2a)
-    end
-end
-quadratic_solve_upper(c, b, a) = begin
-    (a == 0) && return 0.
-    v = b^2 - 4a*c
-    v < 0 ? -b/a : (-b + sqrt(v)) / 2a
-end
-quadratic_solve_lower(c, b, a) = begin
-    (a == 0) && return 0.
-    v = b^2 - 4a*c
-    v < 0 ? -b/a : (-b - sqrt(v)) / 2a
 end
 
 updatetags!(d, ::Val{:Solve}; _...) = begin
-    #HACK: `end` causes missing variable dependency
-    #!haskey(d, :order) && (d[:order] = :(:end))
     !haskey(d, :lower) && (d[:lower] = -Inf)
     !haskey(d, :upper) && (d[:upper] = Inf)
 end
@@ -98,95 +60,46 @@ genupdate(v::VarInfo, ::Val{:Solve}, ::MainStep) = begin
     U = gettag(v, :unit)
     isnothing(U) && (U = @q(u"NoUnits"))
     P = genpolynomial(v)
-    PU = genpolynomialunits(U, length(P))
-    #HACK: pick last one as it seems align with our use (i.e. upper for hs, lower for J)
-    order = gettag(v, :order, :end)
+    n = length(P)
+    PU = genpolynomialunits(U, n)
     lower = gettag(v, :lower)
     upper = gettag(v, :upper)
-    @gensym r a b l
-    # body = @q let $r = $C.solvepolynomial([$(esc.(P)...)], [$(PU...)], $U, $order)#,
-    #               # $a = $C.unitfy($C.value($lower), $U),
-    #               # $b = $C.unitfy($C.value($upper), $U)
-    #     # $l = filter(x -> $a <= x <= $b, $r)
-    #     # isempty($l) && ($l = clamp.($r, $a, $b))
-    #     # #$l = $r
-    #     # $l[$order]
-    #     $r
-    # end
-    @gensym p pu sp
-    # body = @q let $p = [$(esc.(P)...)],
-    #               $pu = [$(PU...)]
-    #     $sp = [deunitfy(q, qu) for (q, qu) in zip($p, $pu)]
-    #     $r = if $order == 1
-    #         quadratic_solve_upper($sp...)
-    #     elseif $order == 2
-    #         quadratic_solve_lower($sp...)
-    #     end
-    #     $r
-    # end
-    @gensym a b c
-    # body = @q let $p = [$(esc.(P)...)],
-    #               $pu = [$(PU...)],
-    #               $c = deunitfy($p[1], $pu[1]),
-    #               $b = deunitfy($p[2], $pu[2]),
-    #               $a = deunitfy($p[3], $pu[3])
-    #     #$sp = [deunitfy(q, qu) for (q, qu) in zip($p, $pu)]
-    #     #$sp = [deunitfy($p[i], $pu[i]) for i in 1:$(length(P))]
-    #     $r = if $order == 1
-    #         #quadratic_solve_upper($sp...)
-    #         quadratic_solve_upper($c, $b, $a)
-    #     elseif $order == 2
-    #         #quadratic_solve_lower($sp...)
-    #         quadratic_solve_lower($c, $b, $a)
-    #     end
-    #     $r
-    # end
-    # body = @q let $c = deunitfy($(esc(P[1])), $(PU[1])),
-    #               $b = deunitfy($(esc(P[2])), $(PU[2])),
-    #               $a = deunitfy($(esc(P[3])), $(PU[3]))
-    #     $r = if $order == 1
-    #         #quadratic_solve_upper($sp...)
-    #         quadratic_solve_upper($c, $b, $a)
-    #     elseif $order == 2
-    #         #quadratic_solve_lower($sp...)
-    #         quadratic_solve_lower($c, $b, $a)
-    #     end
-    #     $r
-    # end
-    # body = @q let $c = deunitfy($(esc(P[1])), $(PU[1])),
-    #               $b = deunitfy($(esc(P[2])), $(PU[2])),
-    #               $a = deunitfy($(esc(P[3])), $(PU[3]))
-    #     $r = $C.PolynomialRoots.roots([$c, $b, $a])
-    #     $l = real.(filter(isreal, $r))
-    #     $l[end]
-    # end
-    # @gensym xl xu a b c r l
-    # body = @q let $c = deunitfy($(esc(P[1])), $(PU[1])),
-    #               $b = deunitfy($(esc(P[2])), $(PU[2])),
-    #               $a = deunitfy($(esc(P[3])), $(PU[3])),
-    #               $xl = $C.unitfy($C.value($lower), $U),
-    #               $xu = $C.unitfy($C.value($upper), $U)
-    #   $r = $C.unitfy(quadratic_solve($c, $b, $a), $U)
-    #   $l = filter(x -> $xl <= x <= $xu, $r)
-    #   isempty($l) && ($l = clamp.($r, $xl, $xu))
-    #   $l[1]
-    # end
-    @gensym xl xu a b c r l x
-    body = @q let $c = deunitfy($(esc(P[1])), $(PU[1])),
-                  $b = deunitfy($(esc(P[2])), $(PU[2])),
-                  $a = deunitfy($(esc(P[3])), $(PU[3])),
-                  $xl = $C.unitfy($C.value($lower), $U),
-                  $xu = $C.unitfy($C.value($upper), $U)
-      $l = $C.unitfy(quadratic_solve2($c, $b, $a), $U)
-      $r = nothing
-      for $x in $l
-          if $xl <= $x <= $xu
-              $r = $x
-              break
+    body = if n == 2 # linear
+        @gensym a b xl xu
+        @q let $a = $(esc(P[2])),
+               $b = $(esc(P[1])),
+               $xl = $C.unitfy($C.value($lower), $U),
+               $xu = $C.unitfy($C.value($upper), $U)
+            clamp(-$b / $a, $xl, $xu)
+        end
+    elseif n == 3 # quadratic
+        @gensym a b c xl xu X r x
+        @q let $a = $C.deunitfy($(esc(P[3])), $(PU[3])),
+               $b = $C.deunitfy($(esc(P[2])), $(PU[2])),
+               $c = $C.deunitfy($(esc(P[1])), $(PU[1])),
+               $xl = $C.unitfy($C.value($lower), $U),
+               $xu = $C.unitfy($C.value($upper), $U)
+          $X = $C.unitfy($C.solvequadratic($a, $b, $c), $U)
+          $r = nothing
+          for $x in $X
+              if $xl <= $x <= $xu
+                  $r = $x
+                  break
+              end
           end
-      end
-      isnothing($r) && ($r = clamp($l[1], $xl, $xu))
-      $r
+          isnothing($r) && ($r = clamp($X[1], $xl, $xu))
+          $r
+        end
+    else # generic polynomials (slow!)
+        @gensym X xl xu l
+        @q let $X = $C.unitfy($C.solvepolynomial([$(esc.(P)...)], [$(PU...)]), $U),
+               $xl = $C.unitfy($C.value($lower), $U),
+               $xu = $C.unitfy($C.value($upper), $U)
+            $l = filter(x -> $xl <= x <= $xu, $X)
+            #TODO: better report error instead of silent clamp?
+            isempty($l) && ($l = clamp.($X, $xl, $xu))
+            $l[end]
+        end
     end
     val = genfunc(v, body)
     genstore(v, val)
--- test/state/solve.jl
+++ test/state/solve.jl
@@ -44,15 +44,49 @@
         @test s.x' == -100u"cm"
     end
 
-    @testset "order" begin
-        @system SSolveRootOrder1(Controller) begin
-            x => (x^2 + 2x - 3) ~ solve(order=1)
+    @testset "linear" begin
+        @system SSolveLinear(Controller) begin
+            x => (2x ⩵ 1) ~ solve
         end
-        @system SSolveRootOrder2(Controller) begin
-            x => (x^2 + 2x - 3) ~ solve(order=2)
+        s = instance(SSolveLinear)
+        @test s.x' == 0.5
+    end
+
+    @testset "quadratic single" begin
+        @system SSolveQuadraticSingle(Controller) begin
+            x => (x^2 ⩵ 1) ~ solve
         end
-        s1 = instance(SSolveRootOrder1)
-        s2 = instance(SSolveRootOrder2)
-        @test Set([s1.x', s2.x']) == Set([1, -3])
+        s = instance(SSolveQuadraticSingle)
+        @test s.x' == 1
+    end
+
+    @testset "quadratic double" begin
+        @system SSolveQuadraticDouble(Controller) begin
+            l ~ preserve(parameter)
+            u ~ preserve(parameter)
+            x => (x^2 + 2x - 3) ~ solve(lower=l, upper=u)
+        end
+        s1 = instance(SSolveQuadraticDouble, config=:SSolveQuadraticDouble => (l=0, u=Inf))
+        @test s1.x' == 1
+        s2 = instance(SSolveQuadraticDouble, config=:SSolveQuadraticDouble => (l=-Inf, u=0))
+        @test s2.x' == -3
+    end
+
+    @testset "cubic" begin
+        @system SSolveCubic(Controller) begin
+            l ~ preserve(parameter)
+            u ~ preserve(parameter)
+            x => ((x-5)*(x-15)*(x-25)) ~ solve(lower=l, upper=u)
+        end
+        s0 = instance(SSolveCubic, config=:SSolveCubic => (l=-Inf, u=0))
+        @test s0.x' == 0
+        s1 = instance(SSolveCubic, config=:SSolveCubic => (l=-Inf, u=10))
+        @test s1.x' ≈ 5
+        s2 = instance(SSolveCubic, config=:SSolveCubic => (l=10, u=20))
+        @test s2.x' ≈ 15
+        s3 = instance(SSolveCubic, config=:SSolveCubic => (l=20, u=30))
+        @test s3.x' ≈ 25
+        s4 = instance(SSolveCubic, config=:SSolveCubic => (l=30, u=Inf))
+        @test s4.x' == 30
     end
 end
