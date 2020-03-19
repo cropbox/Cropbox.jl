ed2d55d3293e8bf7737bb4cf14993b39c107bcda
Kyungdahm Yun <tomyun@gmail.com>
2020-03-19 14:58:03 -0700

    Show variable name in the logs

--- src/state/bisect.jl
+++ src/state/bisect.jl
@@ -63,7 +63,7 @@ genupdate(v::VarInfo, ::Val{:Bisect}, ::MainStep) = begin
         else
             $s.N += 1
             if $s.N > $maxiter
-                @warn "bisect[$($s.N)]: convergence failed!" c=$s.c fc=$s.fc d=$s.d $(v.name)=$C.value($s)
+                @warn "bisect: $($v.name) [$($s.N)] convergence failed!" a=$s.a b=$s.b c=$s.c fa=$s.fa fb=$s.fb fc=$s.fc d=$s.d $(v.name)=$C.value($s)
                 $s.step = :z
                 @goto $lexit
             end
@@ -71,9 +71,9 @@ genupdate(v::VarInfo, ::Val{:Bisect}, ::MainStep) = begin
         if $s.step == :a
             $s.fa = $(genfunc(v))
             if isnan($s.fa)
-                @warn "bisect[$($s.N)]: $($s.a) => $($s.fa)"
+                @warn "bisect: $($v.name) [$($s.N)] $($s.a) => $($s.fa)"
             else
-                @debug "bisect[$($s.N)]: $($s.a) => $($s.fa)"
+                @debug "bisect: $($v.name) [$($s.N)] $($s.a) => $($s.fa)"
             end
             $s.step = :b
             $C.store!($s, $s.b)
@@ -81,16 +81,16 @@ genupdate(v::VarInfo, ::Val{:Bisect}, ::MainStep) = begin
         elseif $s.step == :b
             $s.fb = $(genfunc(v))
             if isnan($s.fb)
-                @warn "bisect[$($s.N)]: $($s.b) => $($s.fb)"
+                @warn "bisect: $($v.name) [$($s.N)] $($s.b) => $($s.fb)"
             else
-                @debug "bisect[$($s.N)]: $($s.b) => $($s.fb)"
+                @debug "bisect: $($v.name) [$($s.N)] $($s.b) => $($s.fb)"
             end
             if sign($s.fa) == sign($s.fb)
                 #HACK: try expanding bracket
                 #$s.N += round(Int, 0.1*$maxiter)
                 $Δ = ($s.b - $s.a) / 2
                 if iszero($Δ)
-                    @warn "bisect[$($s.N)]: expansion failed!" $(v.name)=$C.value($s)
+                    @warn "bisect: $($v.name) [$($s.N)] expansion failed!" $(v.name)=$C.value($s)
                     $s.step = :z
                     @goto $lexit
                 end
@@ -98,7 +98,7 @@ genupdate(v::VarInfo, ::Val{:Bisect}, ::MainStep) = begin
                 #TODO: check no expansion case where Δ gets clipped by min/max
                 $s.a = $(genminmax(v, @q $s.a - $Δ))
                 $s.b = $(genminmax(v, @q $s.b + $Δ))
-                @debug "bisect[$($s.N)]: $($s.a) <- a, b -> $($s.b) "
+                @debug "bisect: $($v.name) [$($s.N)] $($s.a) <- a, b -> $($s.b) "
                 $s.step = :a
                 $C.store!($s, $s.a)
                 @goto $lstart
@@ -110,23 +110,23 @@ genupdate(v::VarInfo, ::Val{:Bisect}, ::MainStep) = begin
         elseif $s.step == :c
             $s.fc = $(genfunc(v))
             if isnan($s.fc)
-                @warn "bisect[$($s.N)]: $($s.c) => $($s.fc)"
+                @warn "bisect: $($v.name) [$($s.N)] $($s.c) => $($s.fc)"
             else
-                @debug "bisect[$($s.N)]: $($s.c) => $($s.fc)"
+                @debug "bisect: $($v.name) [$($s.N)] $($s.c) => $($s.fc)"
             end
             if $s.fc ≈ zero($s.fc) || ($s.b - $s.a) / $s.d < $tol
-                @debug "bisect[$($s.N)]: finished! $($C.value($s))"
+                @debug "bisect: $($v.name) [$($s.N)] finished! $($C.value($s))"
                 $s.step = :z
                 @goto $lexit
             end
             if sign($s.fc) == sign($s.fa)
                 $s.a = $s.c
                 $s.fa = $s.fc
-                @debug "bisect[$($s.N)]: a <- $($s.c)"
+                @debug "bisect: $($v.name) [$($s.N)] a <- $($s.c)"
             else
                 $s.b = $s.c
                 $s.fb = $s.fc
-                @debug "bisect[$($s.N)]: b <- $($s.c)"
+                @debug "bisect: $($v.name) [$($s.N)] b <- $($s.c)"
             end
             $s.c = ($s.a + $s.b) / 2
             $C.store!($s, $s.c)
