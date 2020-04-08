979032211d54caa547df0ced228a51e6d26f7b97
Kyungdahm Yun <tomyun@gmail.com>
2020-04-08 02:03:14 -0700

    Fix placeholder overwriting explicit config

--- src/system/controller.jl
+++ src/system/controller.jl
@@ -6,7 +6,7 @@ end
 instance(S::Type{<:System}; config=(), kwargs...) = begin
     c = configure(config)
     #HACK: support placeholder (0) for the controller name
-    c = Config((k == Symbol(0) ? nameof(S) : k) => v for (k, v) in c)
+    c = configure([(k == Symbol(0) ? nameof(S) : k) => v for (k, v) in c])
     s = S(; config=c, kwargs...)
     update!(s)
 end
