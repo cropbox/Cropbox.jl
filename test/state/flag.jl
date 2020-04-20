940ea510879f0262a4db9ee95d22a4c6723e23a1
Kyungdahm Yun <tomyun@gmail.com>
2020-04-20 16:08:11 -0700

    Add test case for comparing flag vs. track::Bool

--- test/state/flag.jl
+++ test/state/flag.jl
@@ -7,4 +7,26 @@
         s = instance(SFlag)
         @test s.a' == true && s.b' == false
     end
+
+    @testset "flag vs track" begin
+        @system SFlagVsTrack(Controller) begin
+            a => 1 ~ accumulate
+            f1(a) => a >= 1 ~ flag
+            f2(a) => a >= 1 ~ track::Bool
+            x1(f1) => (f1 ? 1 : 0) ~ track
+            x2(f2) => (f2 ? 1 : 0) ~ track
+        end
+        s = instance(SFlagVsTrack)
+        @test s.a' == 0
+        @test s.f1' == false && s.f2' == false
+        @test s.x1' == 0 && s.x2' == 0
+        update!(s)
+        @test s.a' == 1
+        @test s.f1' == true && s.f2' == true
+        @test s.x1' == 0 && s.x2' == 1
+        update!(s)
+        @test s.a' == 2
+        @test s.f1' == true && s.f2' == true
+        @test s.x1' == 1 && s.x2' == 1
+    end
 end
