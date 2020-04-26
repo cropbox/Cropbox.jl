33c47e1f30d7630a58ed5a19cc8f0a05e5473693
Kyungdahm Yun <tomyun@gmail.com>
2020-04-25 21:00:33 -0700

    Make N (leaf_nitrogen_content) a parameter

--- test/gasexchange/base.jl
+++ test/gasexchange/base.jl
@@ -21,7 +21,7 @@
 end
 
 @system NitrogenDependence begin
-    N: nitrogen ~ hold
+    N: leaf_nitrogen_content ~ hold
 
     s => 2.9 ~ preserve(u"m^2/g", parameter)
     N0 => 0.25 ~ preserve(u"g/m^2", parameter)
--- test/gasexchange/nitrogen.jl
+++ test/gasexchange/nitrogen.jl
@@ -2,7 +2,7 @@
     SPAD: SPAD_greenness ~ preserve(parameter)
     SNa: SPAD_N_coeff_a ~ preserve(u"g/m^2", parameter)
     SNb: SPAD_N_coeff_b ~ preserve(u"g/m^2", parameter)
-    N(SPAD, a=SNa, b=SNb): nitrogen => begin
+    N(SPAD, a=SNa, b=SNb): leaf_nitrogen_content => begin
         a*SPAD + b
-    end ~ track(u"g/m^2")
+    end ~ preserve(u"g/m^2", parameter)
 end
