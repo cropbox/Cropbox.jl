0753a5aa05926456fd1e9bbefdce52c1bbab93ee
Kyungdahm Yun <tomyun@gmail.com>
2020-07-08 22:33:22 -0700

    Implement graph rendering with Graphviz

--- src/graph.jl
+++ src/graph.jl
@@ -21,3 +21,51 @@ Base.write(filename::AbstractString, g::Graph; plotopts...) = begin
     f = TikzPictures.PDF(string(filename))
     TikzPictures.save(f, plot(g; plotopts...))
 end
+
+makedot(g::Graph) = begin
+    node(i, l) = """$i [label="$l"]\n"""
+    N = [node(i, l) for (i, l) in enumerate(labels(g))]
+    
+    edge(a, b) = """$a -> $b [style="$(get(ES, (a, b), ""))"]\n"""
+    ES = edgestyles(g)
+    E = [edge(e.src, e.dst) for e in edges(graph(g))]
+    
+    """
+    digraph {
+    node[
+        width=0
+        height=0
+        margin=0.03
+        shape=plaintext
+    ]
+    edge [
+        arrowsize=0.2
+        penwidth=0.5
+    ]
+    $(N...)
+    $(E...)
+    }
+    """
+end
+
+writedot(g::Graph) = let f = "$(tempname()).dot"; 
+    write(f, makedot(g))
+    f
+end
+
+import Graphviz_jll
+writesvg(name::AbstractString, g::Graph) = begin
+    !endswith(name, ".svg") && (name *= ".svg")
+    dot = writedot(g)
+    Graphviz_jll.dot() do exe
+        cmd = `$exe -Tsvg $dot -o $name`
+        success(cmd) || error("cannot execute: $cmd")
+    end
+    name
+end
+
+Base.show(io::IO, ::MIME"image/svg+xml", g::Graph) = begin
+    f = writesvg(tempname(), g)
+    s = read(f, String)
+    print(io, s)
+end
