48a3fae9180b5bf48dd77f397bccf83fbc41ec08
Kyungdahm Yun <tomyun@gmail.com>
2020-07-13 11:10:34 -0700

    Implement value view step in dive()

--- src/util/dive.jl
+++ src/util/dive.jl
@@ -1,5 +1,6 @@
 #TODO: single-option menu from REPL.TerminalMenus: https://github.com/JuliaLang/julia/pull/36369
 import TerminalMenus
+import REPL.Terminals
 import Crayons.Box
 
 struct MenuItem
@@ -30,8 +31,8 @@ dive(s::System, t) = begin
     dive(map(a -> MenuItem(string(a), label(a), s[a]), l), t)
 end
 dive(s::Vector{<:System}, t) = dive(map(t -> MenuItem(string(t[1]), "", t[2]), enumerate(s)), t)
-dive(s::Tabulate, t) = throw(s')
-dive(s::Call, t) = throw(s')
+dive(s::Tabulate, t) = dive(s', t)
+dive(s::Call, t) = dive(s', t)
 dive(s::Produce, t) = dive(map(t -> MenuItem(string(t[1]), "", t[2]), enumerate(s')), t)
 dive(s::State, t) = dive(map(v -> MenuItem("", "", v), s'), t)
 dive(l::Vector{MenuItem}, t) = begin
@@ -49,7 +50,24 @@ dive(l::Vector{MenuItem}, t) = begin
         end
     end
 end
-dive(v, t) = throw(value(v))
+dive(m::MenuItem, t) = dive(value(m), t)
+dive(v, t) = begin
+    term = TerminalMenus.terminal
+    i = term.in_stream
+    o = term.out_stream
+    println(o, t)
+    show(o, MIME("text/plain"), v)
+    print(o, "\n^ ")
+    Terminals.raw!(term, true) && print(o, "\x1b[?25l") # hide cursor
+    c = TerminalMenus.readKey(i)
+    Terminals.raw!(term, false) && print(o, "\x1b[?25h") # unhide cursor
+    println(o)
+    if c == 13 # enter
+        throw(v)
+    elseif c == 3 # ctrl-c
+        throw(InterruptException())
+    end
+end
 
 dive(s::System) = begin
     try
