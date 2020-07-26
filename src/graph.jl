17ca2725e36bedab2ec4e8446e1e396234dd4eb9
Kyungdahm Yun <tomyun@gmail.com>
2020-07-26 00:31:54 -0700

    Remove TikzGraphs/Pictures dependency

--- src/graph.jl
+++ src/graph.jl
@@ -4,24 +4,6 @@ graph(g::Graph) = g
 labels(g::Graph; kw...) = [] #error("labels() not defined for $x")
 edgestyles(g::Graph; kw...) = Dict()
 
-import TikzGraphs
-plot(g::Graph, layout=(), label=(), edgestyle=()) = begin
-    TikzGraphs.plot(
-        graph(g),
-        TikzGraphs.Layouts.Layered(; layout...),
-        labels(g; label...);
-        edge_styles=edgestyles(g; edgestyle...),
-        prepend_preamble="\\usetikzlibrary{backgrounds}",
-        options="background rectangle/.style={fill=white}, show background rectangle",
-    )
-end
-
-import TikzPictures
-Base.write(filename::AbstractString, g::Graph; plotopts...) = begin
-    f = TikzPictures.PDF(string(filename))
-    TikzPictures.save(f, plot(g; plotopts...))
-end
-
 makedot(g::Graph) = begin
     node(i, l) = """$i [label="$l"]\n"""
     N = [node(i, l) for (i, l) in enumerate(labels(g))]
