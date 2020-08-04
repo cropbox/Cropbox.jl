edec721ac4c0b86895d601567681146345b82905
Kyungdahm Yun <tomyun@gmail.com>
2020-08-03 19:53:18 -0700

    Refactor StoreBase from DataFrameStore

--- src/system/store.jl
+++ src/system/store.jl
@@ -1,12 +1,18 @@
-import DataFrames: DataFrame, DataFrameRow, DataFrames
+import DataFrames: DataFrames, DataFrame
 import CSV
 
-@system DataFrameStore begin
+@system StoreBase begin
     filename => "" ~ preserve::String(parameter)
     ik: indexkey => :index ~ preserve::Symbol(parameter)
 
     i(t=nounit(context.clock.tick)): index => t + 1 ~ track::Int
-    ix(; r::DataFrameRow): indexer => DataFrames.row(r) ~ call
+    ix: indexer ~ hold
+
+    s: store ~ hold
+end
+
+@system DataFrameStore(StoreBase) begin
+    ix(; r::DataFrames.DataFrameRow): indexer => DataFrames.row(r) ~ call::Int
 
     df(filename, ik, ix): dataframe => begin
         df = CSV.read(filename)
@@ -20,7 +26,7 @@ import CSV
 
     s(gdf, i): store => begin
         gdf[(i,)][1, :]
-    end ~ track::DataFrameRow{DataFrame,DataFrames.Index}
+    end ~ track::DataFrames.DataFrameRow{DataFrame,DataFrames.Index}
 end
 
 export DataFrameStore
