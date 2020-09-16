a90f4e4c8c0d6e6a5a3360ae7da2fb7bc86990c7
Kyungdahm Yun <tomyun@gmail.com>
2020-09-15 18:22:53 -0700

    Use approximate comparison for some integrals

--- test/state/integrate.jl
+++ test/state/integrate.jl
@@ -11,9 +11,9 @@
         s2 = instance(SIntegrate, config=:0 => :w => 2)
         @test s2.f' == 4
         s3 = instance(SIntegrate, config=:0 => :a => π/2)
-        @test s3.f' == 1
+        @test s3.f' ≈ 1
         s4 = instance(SIntegrate, config=:0 => (a = π, b = 2π))
-        @test s4.f' == -2
+        @test s4.f' ≈ -2
     end
 
     @testset "unit" begin
