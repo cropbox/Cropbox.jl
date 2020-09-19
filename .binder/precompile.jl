0c6c82067ad8e0d927c4e407046ad08d25f65ae5
Kyungdahm Yun <tomyun@gmail.com>
2020-09-18 22:47:35 -0700

    Include SVG() as well as SVGJS()

--- .binder/precompile.jl
+++ .binder/precompile.jl
@@ -8,6 +8,6 @@ end
 r = simulate(S);
 plot(r, :tick, :c; backend=:UnicodePlots) |> display;
 plot(r, :tick, :c; backend=:Gadfly)[] |> Cropbox.Gadfly.SVGJS() |> display;
-plot(r, :tick, :c; kind=:line, backend=:Gadfly)' |> Cropbox.Gadfly.SVGJS() |> display;
+plot(r, :tick, :c; kind=:line, backend=:Gadfly)' |> Cropbox.Gadfly.SVG() |> display;
 visualize(S, :tick, :c; backend=:UnicodePlots) |> display;
 visualize(S, :tick, :c; backend=:Gadfly)[] |> Cropbox.Gadfly.SVGJS() |> display;
