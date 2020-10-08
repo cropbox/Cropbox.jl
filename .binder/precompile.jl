aa33ec19d35e788faa8de483020fd8d30cbb41f4
Kyungdahm Yun <tomyun@gmail.com>
2020-10-07 18:15:29 -0700

    Remove unnecessary output hiding (;)

--- .binder/precompile.jl
+++ .binder/precompile.jl
@@ -5,12 +5,12 @@ using Cropbox
     b(a) ~ track
     c(b) ~ accumulate
 end
-r = simulate(S);
-plot(r, :tick, :c; backend=:UnicodePlots) |> display;
-plot(r, :tick, :c; backend=:Gadfly)[] |> Cropbox.Gadfly.SVGJS() |> display;
-plot(r, :tick, :c; kind=:line, backend=:Gadfly)' |> Cropbox.Gadfly.SVG() |> display;
-visualize(S, :tick, :c; backend=:UnicodePlots) |> display;
-visualize(S, :tick, :c; backend=:Gadfly)[] |> Cropbox.Gadfly.SVGJS() |> display;
+r = simulate(S)
+plot(r, :tick, :c; backend=:UnicodePlots) |> display
+plot(r, :tick, :c; backend=:Gadfly)[] |> Cropbox.Gadfly.SVGJS() |> display
+plot(r, :tick, :c; kind=:line, backend=:Gadfly)' |> Cropbox.Gadfly.SVG() |> display
+visualize(S, :tick, :c; backend=:UnicodePlots) |> display
+visualize(S, :tick, :c; backend=:Gadfly)[] |> Cropbox.Gadfly.SVGJS() |> display
 
 using Test
 
