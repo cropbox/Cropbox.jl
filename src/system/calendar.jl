a0fbb98f8aac1f447f2e28f64236d8d317701121
Kyungdahm Yun <tomyun@gmail.com>
2020-11-11 23:55:38 -0800

    Prevent inexact error when calculating seconds

--- src/system/calendar.jl
+++ src/system/calendar.jl
@@ -4,7 +4,7 @@ import Dates
 @system Calendar begin
     init => ZonedDateTime(2017, 7, 16, tz"America/Los_Angeles") ~ preserve::ZonedDateTime(extern, parameter)
     last => nothing ~ preserve::ZonedDateTime(extern, parameter, optional)
-    time(t0=init, t=context.clock.tick) => t0 + (Cropbox.deunitfy(t, u"s") |> Dates.Second) ~ track::ZonedDateTime
+    time(t0=init, t=context.clock.tick) => t0 + (Cropbox.deunitfy(t, u"s") |> round |> Dates.Second) ~ track::ZonedDateTime
     stop(time, last) => begin
         isnothing(last) ? false : (time >= last)
     end ~ track::Bool
