33b7c8880ca7dfef0e7ac03579edf5b2bea060a5
Kyungdahm Yun <tomyun@gmail.com>
2020-12-06 12:02:46 -0800

    Re-enable explicit continuous scale

--- src/util/plot.jl
+++ src/util/plot.jl
@@ -238,9 +238,8 @@ plot2!(::Val{:Gadfly}, p::Union{Plot,Nothing}, X, Ys; kind, title, xlab, ylab, l
 
     if isnothing(p)
         scales = [
-            #TODO: fix performance regression with custom system image
-            #Gadfly.Scale.x_continuous,
-            #Gadfly.Scale.y_continuous,
+            Gadfly.Scale.x_continuous(),
+            Gadfly.Scale.y_continuous(),
             Gadfly.Coord.cartesian(xmin=xlim[1], ymin=ylim[1], xmax=xlim[2], ymax=ylim[2], aspect_ratio=aspect),
         ]
         guides = [
@@ -372,9 +371,8 @@ plot3!(::Val{:Gadfly}, X, Y, Z; kind, title, legend, legendpos, xlab, ylab, zlab
     labels = isnothing(zlabgap) ? () : label.(zlim[1]:zlabgap:zlim[2])
 
     scales = [
-        #TODO: fix performance regression with custom system image
-        #Gadfly.Scale.x_continuous,
-        #Gadfly.Scale.y_continuous,
+        Gadfly.Scale.x_continuous(),
+        Gadfly.Scale.y_continuous(),
         Gadfly.Scale.color_continuous(minvalue=zlim[1], maxvalue=zlim[2]),
         Gadfly.Coord.cartesian(xmin=xlim[1], ymin=ylim[1], xmax=xlim[2], ymax=ylim[2], aspect_ratio=aspect),
     ]
