f0c7190f458a5690dc24929c8960b0e0a74fdff7
Kyungdahm Yun <tomyun@gmail.com>
2020-12-08 12:52:03 -0800

    Consolidate plot backend into separate files

---
+++ src/util/plot/Gadfly.jl
@@ -0,0 +1,177 @@
+import Gadfly
+
+plot2!(::Val{:Gadfly}, p::Union{Plot,Nothing}, X, Ys; kind, title, xlab, ylab, legend, legendpos, names, colors, xlim, ylim, xunit, yunit, aspect) = begin
+    n = length(Ys)
+    Xs = [X for _ in 1:n]
+    kinds = [kind for _ in 1:n]
+
+    if kind == :line
+        geoms = [Gadfly.Geom.line]
+    elseif kind == :scatter
+        geoms = [Gadfly.Geom.point]
+    elseif kind == :scatterline
+        geoms = [Gadfly.Geom.point, Gadfly.Geom.line]
+    else
+        error("unrecognized plot kind = $kind")
+    end
+
+    #HACK: manual_color_key() expects [] while colorkey() expects nothing
+    keypos = isnothing(legendpos) ? [] : legendpos .* [Gadfly.w, Gadfly.h]
+
+    theme = Gadfly.Theme(
+        background_color="white",
+        plot_padding=[5*Gadfly.mm, 5*Gadfly.mm, 5*Gadfly.mm, 0*Gadfly.mm],
+        major_label_font_size=10*Gadfly.pt,
+        key_title_font_size=9*Gadfly.pt,
+        key_position=isempty(keypos) ? :right : :inside,
+        point_size=0.7*Gadfly.mm,
+        discrete_highlight_color=_->Gadfly.RGBA(1, 1, 1, 0),
+    )
+
+    create_colors(colors; n0=0) = begin
+        n = length(colors)
+        C = Gadfly.Scale.default_discrete_colors(n0+n)[n0+begin:end]
+        f(c::Int, _) = p.opt[:colors][c]
+        f(c, _) = parse(Gadfly.Colorant, c)
+        f(::Nothing, i) = C[i]
+        [f(c, i) for (i, c) in enumerate(colors)]
+    end
+    colorkey(colors) = begin
+        NC = filter(x -> let (n, c) = x; !isempty(n) end, collect(zip(names, colors)))
+        if !isempty(NC)
+            N, C = first.(NC), last.(NC)
+            Gadfly.Guide.manual_color_key(legend, N, C; pos=keypos)
+        end
+    end
+    colorkey!(key, colors) = begin
+        k = colorkey(colors)
+        if !isnothing(k)
+            append!(key.labels, k.labels)
+            append!(key.colors, k.colors)
+        end
+        k
+    end
+    update_color!(guides, colors) = begin
+        #TODO: very hacky approach to append new plots... definitely need a better way
+        keys = filter(x -> x isa Gadfly.Guide.ManualDiscreteKey, guides)
+        if isempty(keys)
+            key = colorkey(colors)
+            !isnothing(key) && push!(guides, key)
+        else
+            key = only(keys)
+            colorkey!(key, colors)
+        end
+    end
+    create_layers(colors) = [Gadfly.layer(x=Xs[i], y=Ys[i], geoms..., Gadfly.Theme(theme; default_color=colors[i])) for i in 1:n]
+
+    if isnothing(p)
+        scales = [
+            #TODO: fix performance regression with custom system image
+            #Gadfly.Scale.x_continuous,
+            #Gadfly.Scale.y_continuous,
+            Gadfly.Coord.cartesian(xmin=xlim[1], ymin=ylim[1], xmax=xlim[2], ymax=ylim[2], aspect_ratio=aspect),
+        ]
+        guides = [
+            Gadfly.Guide.title(title),
+            Gadfly.Guide.xlabel(xlab),
+            Gadfly.Guide.ylabel(ylab),
+        ]
+        colors = create_colors(colors)
+        update_color!(guides, colors)
+        layers = create_layers(colors)
+        obj = Gadfly.plot(
+            scales...,
+            guides...,
+            layers...,
+            theme,
+        )
+        p = Plot(obj; Xs, Ys, kinds, colors, title, xlab, ylab, legend, names, xlim, ylim, xunit, yunit, aspect)
+    else
+        obj = p.obj
+        n0 = length(obj.layers)
+        colors = create_colors(colors; n0)
+        update_color!(obj.guides, colors)
+        foreach(l -> Gadfly.push!(obj, l), create_layers(colors))
+        for l in create_layers(colors)
+            Gadfly.push!(obj, l)
+        end
+        update!(p; Xs, Ys, kinds, colors, names)
+    end
+    p
+end
+
+plot3!(::Val{:Gadfly}, X, Y, Z; kind, title, legend, legendpos, xlab, ylab, zlab, xlim, ylim, zlim, zgap, zlabgap, aspect) = begin
+    if kind == :heatmap
+        geom = Gadfly.Geom.rectbin
+        data = (x=X, y=Y, color=Z)
+    elseif kind == :contour
+        levels = isnothing(zgap) ? 100 : collect(zlim[1]:zgap:zlim[2])
+        geom = Gadfly.Geom.contour(; levels)
+        data = (x=X, y=Y, z=Z)
+    else
+        error("unrecognized plot kind = $kind")
+    end
+
+    #HACK: colorkey() expects nothing while manual_color_key() expects []
+    keypos = isnothing(legendpos) ? nothing : legendpos .* [Gadfly.w, Gadfly.h]
+
+    theme = Gadfly.Theme(
+        background_color="white",
+        plot_padding=[5*Gadfly.mm, 5*Gadfly.mm, 5*Gadfly.mm, 0*Gadfly.mm],
+        major_label_font_size=10*Gadfly.pt,
+        key_title_font_size=9*Gadfly.pt,
+        key_position=legend ? isnothing(keypos) ? :right : :inside : :none,
+    )
+
+    label(z) = begin
+        #TODO: remove redundant creation of Scale
+        zmin, zmax = zlim
+        zspan = zmax - zmin
+        scale = Gadfly.Scale.color_continuous(minvalue=zmin, maxvalue=zmax)
+        color = scale.f((z - zmin) / zspan)
+
+        i = findmin(abs.(Z .- z))[2]
+        #HACK: ignore lables presumably out of bound
+        if i == firstindex(Z) || i == lastindex(Z)
+            Gadfly.Guide.annotation(Gadfly.compose(Gadfly.context()))
+        else
+            x, y = X[i], Y[i]
+            Gadfly.Guide.annotation(
+                Gadfly.compose(
+                    Gadfly.context(),
+                    Gadfly.Compose.text(x, y, string(z), Gadfly.hcenter, Gadfly.vcenter),
+                    Gadfly.font(theme.minor_label_font),
+                    Gadfly.fontsize(theme.minor_label_font_size),
+                    Gadfly.fill(color),
+                    Gadfly.stroke("white"),
+                    Gadfly.linewidth(0.1*Gadfly.pt),
+                )
+            )
+        end
+    end
+    labels = isnothing(zlabgap) ? () : label.(zlim[1]:zlabgap:zlim[2])
+
+    scales = [
+        #TODO: fix performance regression with custom system image
+        #Gadfly.Scale.x_continuous,
+        #Gadfly.Scale.y_continuous,
+        Gadfly.Scale.color_continuous(minvalue=zlim[1], maxvalue=zlim[2]),
+        Gadfly.Coord.cartesian(xmin=xlim[1], ymin=ylim[1], xmax=xlim[2], ymax=ylim[2], aspect_ratio=aspect),
+    ]
+    guides = [
+        Gadfly.Guide.title(title),
+        Gadfly.Guide.xlabel(xlab),
+        Gadfly.Guide.ylabel(ylab),
+        Gadfly.Guide.colorkey(title=zlab; pos=keypos),
+    ]
+
+    obj = Gadfly.plot(
+        scales...,
+        guides...,
+        geom,
+        labels...,
+        theme;
+        data...,
+    )
+    Plot(obj; X, Y, Z, kind, title, xlab, ylab, zlab, xlim, ylim, zlim, aspect)
+end
---
+++ src/util/plot/UnicodePlots.jl
@@ -0,0 +1,86 @@
+import UnicodePlots
+
+plot2!(::Val{:UnicodePlots}, p::Union{Plot,Nothing}, X, Ys; kind, title, xlab, ylab, legend, legendpos, names, colors, xlim, ylim, xunit, yunit, aspect, width=40, height=15) = begin
+    canvas = if get(ENV, "GITHUB_ACTIONS", "false") == "true"
+        UnicodePlots.DotCanvas
+    else
+        UnicodePlots.BrailleCanvas
+    end
+
+    if kind == :line || kind == :scatterline
+        plot! = UnicodePlots.lineplot!
+    elseif kind == :scatter
+        plot! = UnicodePlots.scatterplot!
+    else
+        error("unrecognized plot kind = $kind")
+    end
+
+    !isnothing(legendpos) && @warn "unsupported legend position = $legendpos"
+
+    create_colors(colors; n0=0) = begin
+        n = length(colors)
+        C = collect(Iterators.take(Iterators.cycle(UnicodePlots.color_cycle), n+n0))[n0+begin:end]
+        f(c::Int, _) = p.opt[:colors][c]
+        f(c, _) = c in keys(UnicodePlots.color_encode) ? c : :normal
+        f(::Nothing, i) = C[i]
+        [f(c, i) for (i, c) in enumerate(colors)]
+    end
+
+    #HACK: add newline to ensure clearing (i.e. test summary right after plot)
+    !endswith(xlab, "\n") && (xlab *= "\n")
+
+    if isnothing(p)
+        a = Float64[]
+        !isnothing(aspect) && (width = round(Int, aspect * 2height))
+        obj = UnicodePlots.Plot(a, a, canvas; title, xlabel=xlab, ylabel=ylab, xlim, ylim, width, height)
+        UnicodePlots.annotate!(obj, :r, legend)
+        p = Plot(obj; Xs=[], Ys=[], kinds=[], colors=[], title, xlab, ylab, legend, names, xlim, ylim, xunit, yunit, aspect, width, height)
+    end
+    colors = create_colors(colors; n0=length(p.opt[:Ys]))
+    for (i, (Y, name)) in enumerate(zip(Ys, names))
+        #HACK: UnicodePlots can't handle missing
+        Y = coalesce.(Y, NaN)
+        color = colors[i]
+        plot!(p.obj, X, Y; name, color)
+        #TODO: remember colors
+        update!(p; Xs=[X], Ys=[Y], kinds=[kind], colors=[color])
+    end
+    p
+end
+
+plot3!(::Val{:UnicodePlots}, X, Y, Z; kind, title, legend, legendpos, xlab, ylab, zlab, xlim, ylim, zlim, zgap, zlabgap, aspect, width=0, height=30) = begin
+    if kind == :heatmap
+        ;
+    elseif kind == :contour
+        @warn "unsupported plot kind = $kind"
+    else
+        error("unrecognized plot kind = $kind")
+    end
+
+    !legend && @warn "unsupported legend = $legend"
+    !isnothing(legendpos) && @warn "unsupported legend position = $legendpos"
+
+    !isnothing(zgap) && @warn "unsupported contour interval = $zgap"
+    !isnothing(zlabgap) && @warn "unsupported countour label interval = $zlabgap"
+
+    #HACK: add newline to ensure clearing (i.e. test summary right after plot)
+    !endswith(xlab, "\n") && (xlab *= "\n")
+
+    arr(a) = sort(unique(a))
+    x = arr(X)
+    y = arr(Y)
+    M = reshape(Z, length(y), length(x))
+
+    offset(a) = a[1]
+    xoffset = offset(x)
+    yoffset = offset(y)
+    scale(a) = (a[end] - offset(a)) / (length(a) - 1)
+    xscale = scale(x)
+    yscale = scale(y)
+
+    !isnothing(aspect) && (width = round(Int, aspect * height))
+
+    #TODO: support zlim (minz/maxz currentyl fixed in UnicodePlots)
+    obj = UnicodePlots.heatmap(M; title, xlabel=xlab, ylabel=ylab, zlabel=zlab, xscale, yscale, xlim, ylim, xoffset, yoffset, width, height)
+    Plot(obj; X, Y, Z, kind, title, xlab, ylab, zlab, xlim, ylim, zlim, aspect, width, height)
+end
