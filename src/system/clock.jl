4f527fc6af7bb26c1d964a1e008d6302cfe296d7
Kyungdahm Yun <tomyun@gmail.com>
2021-02-28 22:57:26 -0800

    Update Clock with timeunit const/function

--- src/system/clock.jl
+++ src/system/clock.jl
@@ -1,9 +1,13 @@
-@system Clock begin
+abstract type Clock <: System end
+timeunit(::Type{<:Clock}) = u"hr"
+@system Clock{timeunit = timeunit(Clock)} begin
     context ~ ::Nothing
     config ~ ::Config(override)
-    init => 0 ~ preserve(u"hr", parameter)
-    step => 1 ~ preserve(u"hr", parameter)
-    tick => nothing ~ advance(init=init, step=step, unit=u"hr")
+    init => 0 ~ preserve(unit=timeunit, parameter)
+    step => 1 ~ preserve(unit=timeunit, parameter)
+    tick => nothing ~ advance(init=init, step=step, unit=timeunit)
 end
+
+timeunit(c::C) where {C<:Clock} = timeunit(C)
 
 export Clock
--- src/system/context.jl
+++ src/system/context.jl
@@ -4,4 +4,14 @@
     clock(config) ~ ::Clock
 end
 
+timeunit(c::Context) = timeunit(c.clock)
+
+#HACK: explicitly set up timeunit for default Context
+#TODO: merge Context and Clock to remove boilerplates
+timeunit(::Type{Context}) = timeunit(Clock)
+timeunit(::Type{typefor(Context)}) = timeunit(Context)
+
+#HACK: fallback when timeunit() not available for custom Context
+timeunit(C::Type{<:Context}) = only(filter(v -> v.name == :clock, geninfos(C))).type |> scopeof(C).eval |> timeunit
+
 export Context
