7b7ce74521653c8cec36514d212a57ab91b53141
Kyungdahm Yun <tomyun@gmail.com>
2021-03-13 22:47:16 -0800

    Reimplement `advance` state variable
    - Use standard `value` cache instead of relying on varying initial step
    - Remove custom value/advance!()
    - Ensure unitfy for init/step

--- src/state/advance.jl
+++ src/state/advance.jl
@@ -1,34 +1,29 @@
 mutable struct Advance{T} <: State{T}
+    value::T
     t::T
     Δt::T
-    Δ::T
 end
 
 Advance(; init=nothing, step=nothing, unit, _type, _...) = begin
-    T = valuetype(_type, value(unit))
-    t = isnothing(init) ? zero(T) : value(init)
-    #HACK: ensure initial update! not increase tick already
-    Δt = zero(T)
-    Δ = isnothing(step) ? oneunit(T) : value(step)
+    U = value(unit)
+    T = valuetype(_type, U)
+    t = isnothing(init) ? zero(T) : unitfy(value(init), U)
+    Δt = isnothing(step) ? oneunit(T) : unitfy(value(step), U)
     #T = promote_type(typeof(t), typeof(Δt))
-    Advance{T}(t, Δt, Δ)
+    Advance{T}(t, t, Δt)
 end
 
 constructortags(::Val{:Advance}) = (:init, :step, :unit)
-
-value(s::Advance) = s.t
-advance!(s::Advance) = begin
-    s.t += s.Δt
-    s.Δt = s.Δ
-end
 
 genvartype(v::VarInfo, ::Val{:Advance}; V, _...) = @q Advance{$V}
 
 geninit(v::VarInfo, ::Val{:Advance}) = missing
 
 genupdate(v::VarInfo, ::Val{:Advance}, ::MainStep) = begin
-    @gensym s
-    @q let $s = $(symstate(v))
-        $C.advance!($s)
+    @gensym s t
+    @q let $s = $(symstate(v)),
+           $t = $s.t
+        $s.t += $s.Δt
+        $C.store!($s, $t)
     end
 end
