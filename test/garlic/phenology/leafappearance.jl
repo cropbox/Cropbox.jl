33302824c327d336631ce0cf7993c71be32c49d5
Kyungdahm Yun <tomyun@gmail.com>
2021-04-23 14:50:43 -0700

    Make LTAR_SDm/k private parameters

--- test/garlic/phenology/leafappearance.jl
+++ test/garlic/phenology/leafappearance.jl
@@ -1,9 +1,9 @@
 @system LeafAppearance(Stage, Germination, Emergence, LeafInitiation) begin
     LTARa_max: maximum_leaf_tip_appearance_rate_asymptote => 0.4421 ~ preserve(u"d^-1", parameter)
-    LTAR_SDm => 117.7523 ~ preserve(u"d", parameter)
-    LTAR_k => 0.0256 ~ preserve(u"d^-1", parameter)
-    LTAR_max(asym=LTARa_max, x=SD, x_m=LTAR_SDm, k=LTAR_k): maximum_leaf_tip_appearance_rate => begin
-        asym / (1 + exp(-k * (x - x_m)))
+    _SDm => 117.7523 ~ preserve(u"d", parameter)
+    _k => 0.0256 ~ preserve(u"d^-1", parameter)
+    LTAR_max(LTARa_max, SD, _SDm, _k): maximum_leaf_tip_appearance_rate => begin
+        LTARa_max / (1 + exp(-k * (SD - SDm)))
     end ~ preserve(u"d^-1", parameter)
 
     leaf_tip_appearance(r=LTAR_max, β=BF.ΔT) => r*β ~ accumulate(when=leaf_appearing)
