e4ed2a9b6bc1b159ede1834c860e702602ed67a3
Kyungdahm Yun <tomyun@gmail.com>
2021-07-07 15:43:48 -0700

    Add evaluate() for obs and est dataframes

--- src/util/evaluate.jl
+++ src/util/evaluate.jl
@@ -85,4 +85,68 @@ evaluate(S::Type{<:System}, obs, configs; index=nothing, target, metric=nothing,
     cost()
 end
 
+"""
+    evaluate(obs, est; <keyword arguments>) -> Number | Tuple
+
+Compare observation data `obs` and estimation data `est` with a choice of evaluation metric.
+
+# Arguments
+- `obs::DataFrame`: observation data to be used for evaluation.
+- `est::DataFrame`: estimated data from simulation.
+
+# Keyword Arguments
+## Layout
+- `index`: variables referring to index columns of the output.
+- `target`: variables referring to non-index columns of the output.
+
+## Evaluation
+- `metric=nothing`: evaluation metric (`:rmse`, `:nrmse`, `:mae`, `:mape`, `:ef`, `:dr`); default is RMSE.
+
+See also: [`evaluate`](@ref)
+
+# Examples
+```julia-repl
+julia> obs = DataFrame(time = [1, 2, 3]u"hr", b = [10, 20, 30]u"g");
+
+julia> est = DataFrame(time = [1, 2, 3]u"hr", b = [10, 20, 30]u"g", c = [11, 19, 31]u"g");
+
+julia> evaluate(obs, est; index = :time, target = :b)
+0.0 g
+
+julia> evaluate(obs, est; index = :time, target = :b => :c)
+1.0 g
+```
+"""
+evaluate(obs::DataFrame, est::DataFrame; index=nothing, target, metric=nothing, kwargs...) = begin
+    S = nothing
+    #HACK: use copy due to normalize!
+    obs = copy(obs)
+    I = parseindex(index, S) |> collect
+    IO = parseindex(index, S) |> keys |> collect
+    TO = parsetarget(target, S) |> keys |> collect
+    TE = parsetarget(target, S) |> values |> collect
+    n = length(TO)
+    multi = n > 1
+    isnothing(metric) && (metric = :rmse)
+    metric = metricfunc(metric)
+    NT = DataFrames.make_unique([propertynames(obs)..., TE...], makeunique=true)
+    TE1 = NT[end-n+1:end]
+    residual() = begin
+        isempty(est) && return repeat([Inf], n)
+        normalize!(obs, est, on=I)
+        df = DataFrames.innerjoin(obs, est, on=I, makeunique=true)
+        df = df[!, [IO..., TO..., TE1...]]
+        DataFrames.dropmissing!(df)
+        r = [(df[!, e], df[!, o]) for (e, o) in zip(TO, TE1)]
+    end
+    cost() = begin
+        R = [residual()]
+        e = map(getindex.(R, i) for i in 1:n) do r
+            metric(vcat(first.(r)...), vcat(last.(r)...))
+        end
+        multi ? Tuple(e) : only(e)
+    end
+    cost()
+end
+
 export evaluate
