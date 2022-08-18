c774b0c5bdfbb8efcdec9a4bae8981a4220cec9f
Kyungdahm Yun <tomyun@gmail.com>
2022-08-18 13:26:43 -0700

    `:nothing` removed in Crayons (KristofferC/Crayons.jl#60)

--- src/util/color.jl
+++ src/util/color.jl
@@ -3,8 +3,8 @@ import Highlights
 
 function Highlights.Format.render(io::IO, ::MIME"text/ansi", tokens::Highlights.Format.TokenIterator)
     for (str, id, style) in tokens
-        fg = style.fg.active ? map(Int, (style.fg.r, style.fg.g, style.fg.b)) : :nothing
-        bg = style.bg.active ? map(Int, (style.bg.r, style.bg.g, style.bg.b)) : :nothing
+        fg = style.fg.active ? map(Int, (style.fg.r, style.fg.g, style.fg.b)) : nothing
+        bg = style.bg.active ? map(Int, (style.bg.r, style.bg.g, style.bg.b)) : nothing
         crayon = Crayons.Crayon(
             foreground = fg,
             background = bg,
