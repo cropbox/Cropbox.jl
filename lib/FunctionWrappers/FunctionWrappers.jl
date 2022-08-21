5f1ac54fd08b13251e6bd088060d215c9537f995
Kyungdahm Yun <tomyun@gmail.com>
2022-08-20 17:32:34 -0700

    Fix cfuntion return type error on Julia 1.8 (yuyichao/FunctionWrappers.jl#25)

--- lib/FunctionWrappers/FunctionWrappers.jl
+++ lib/FunctionWrappers/FunctionWrappers.jl
@@ -31,11 +31,9 @@ end
 
 #HACK: avoid strange LocalScan error on Julia 1.5, 1.7
 # https://github.com/yuyichao/FunctionWrappers.jl/issues/17
-if VERSION >= v"1.8.0"
-    Base.@pure pass_by_value(T) = Base.allocatedinline(T)
-else
-    Base.@pure pass_by_value(T) = isbitstype(T)
-end
+#HACK: avoid cfunction return type error on Julia 1.8
+# https://github.com/yuyichao/FunctionWrappers.jl/issues/25
+Base.@pure pass_by_value(T) = isbitstype(T)
 
 Base.@pure is_singleton(@nospecialize(T)) = isdefined(T, :instance)
 # Base.@pure get_instance(@nospecialize(T)) = Base.getfield(T, :instance)
